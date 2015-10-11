322e97162539538d9dfdea3c664b9bf2963b7b82
Laurent Laville <laurent.laville@gmail.com>
2015-10-11 09:36:23 +0200

    introduced first example to use extension factory

---
+++ examples/useExtensionFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Bartlett\CompatInfoDb\ExtensionFactory;
+
+$dump = false;
+
+$name = 'curl';
+$ext  = new ExtensionFactory($name);
+
+printf('== Latest PHP platforms supported');
+echo PHP_EOL;
+foreach (array('5.2', '5.3', '5.4', '5.5', '5.6') as $phpVersion) {
+    printf('= PHP %s : %s', $phpVersion, ExtensionFactory::getLatestPhpVersion($phpVersion));
+    echo PHP_EOL;
+}
+echo PHP_EOL;
+
+printf('== Details of %s extension', $name);
+echo PHP_EOL;
+
+printf('= Current version : %s', $ext->getCurrentVersion());
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getReleases(), true) : count($ext->getReleases());
+printf('= Releases : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getInterfaces(), true) : count($ext->getInterfaces());
+printf('= Interfaces : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getClasses(), true) : count($ext->getClasses());
+printf('= Classes : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getFunctions(), true) : count($ext->getFunctions());
+printf('= Functions : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getConstants(), true) : count($ext->getConstants());
+printf('= Constants : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getIniEntries(), true) : count($ext->getIniEntries());
+printf('= INI entries : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getClassConstants(), true) : count($ext->getClassConstants());
+printf('= Class Constants : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getClassStaticMethods(), true) : count($ext->getClassStaticMethods());
+printf('= Class Static Methods : %s', $results);
+echo PHP_EOL;
+
+$results = $dump ? print_r($ext->getClassMethods(), true) : count($ext->getClassMethods());
+printf('= Class Methods : %s', $results);
+echo PHP_EOL;
