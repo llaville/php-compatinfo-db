3d253b4d028ddd744bcef4f3b5f4e12ea027a8ab
Laurent Laville <laurent.laville@gmail.com>
2016-03-05 10:20:14 +0100

    remove unused Use Statement and bump new version 1.6

--- data/handleDB.php
+++ data/handleDB.php
@@ -23,7 +23,6 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -910,5 +909,5 @@ class DbHandleApplication extends Application
     }
 }
 
-$application = new DbHandleApplication('Database handler for CompatInfo', '1.5.0');
+$application = new DbHandleApplication('Database handler for CompatInfo', '1.6.0');
 $application->run();
