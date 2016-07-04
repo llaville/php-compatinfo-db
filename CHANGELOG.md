107231cc7908a591cf2b8e92689f0f342c5442ba
Laurent Laville <laurent.laville@gmail.com>
2016-07-04 20:12:05 +0200

    Add support to PHP 7.0.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ using the [Keep a CHANGELOG](http://keepachangelog.com) principles.
 
 ### Added
 
+- Support to PHP 7.0.8
 - Support to PHP 5.6.23
 - Support to PHP 5.5.37
 
--- src/Bartlett/CompatInfoDb/ExtensionFactory.php
+++ src/Bartlett/CompatInfoDb/ExtensionFactory.php
@@ -32,7 +32,7 @@ class ExtensionFactory implements ReferenceInterface
     const LATEST_PHP_5_4 = '5.4.45';
     const LATEST_PHP_5_5 = '5.5.37';
     const LATEST_PHP_5_6 = '5.6.23';
-    const LATEST_PHP_7_0 = '7.0.7';
+    const LATEST_PHP_7_0 = '7.0.8';
 
     protected $storage;
 
