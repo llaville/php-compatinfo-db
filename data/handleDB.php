48515939d84c566871e876efc04b7a1b7ac4c8db
Laurent Laville <laurent.laville@gmail.com>
2016-12-15 22:05:41 +0100

    Bump new version 1.16

--- data/handleDB.php
+++ data/handleDB.php
@@ -910,5 +910,5 @@ class DbHandleApplication extends Application
     }
 }
 
-$application = new DbHandleApplication('Database handler for CompatInfo', '1.15.0');
+$application = new DbHandleApplication('Database handler for CompatInfo', '1.16.0');
 $application->run();
