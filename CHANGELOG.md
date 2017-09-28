d92a81913fac9dc7af272fc7931ec4e7ba1441c4
Laurent Laville <laurent.laville@gmail.com>
2017-09-28 23:33:23 +0200

    Add support to PHP 7.1.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ using the [Keep a CHANGELOG](http://keepachangelog.com) principles.
 
 ### Added
 
+- Support to PHP 7.1.10
 - Support to PHP 7.0.24
 
 ### Changed
--- src/Bartlett/CompatInfoDb/ExtensionFactory.php
+++ src/Bartlett/CompatInfoDb/ExtensionFactory.php
@@ -33,7 +33,7 @@ class ExtensionFactory implements ReferenceInterface
     const LATEST_PHP_5_5 = '5.5.38';
     const LATEST_PHP_5_6 = '5.6.31';
     const LATEST_PHP_7_0 = '7.0.24';
-    const LATEST_PHP_7_1 = '7.1.9';
+    const LATEST_PHP_7_1 = '7.1.10';
 
     protected $storage;
 
