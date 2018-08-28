8fc132bbf1acb0d49d09a0de0b99a49b643943ec
Laurent Laville <laurent.laville@gmail.com>
2018-08-28 21:14:43 +0200

    tag new version 1.35

--- data/handleDB.php
+++ data/handleDB.php
@@ -1984,5 +1984,5 @@ class DbHandleApplication extends Application
     }
 }
 
-$application = new DbHandleApplication('Database handler for CompatInfo', '1.34.0');
+$application = new DbHandleApplication('Database handler for CompatInfo', '1.35.0');
 $application->run();
