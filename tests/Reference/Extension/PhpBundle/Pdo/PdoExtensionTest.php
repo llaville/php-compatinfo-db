78e48ec3856f75505f3e45f91b7e7b8d5e333aac
Laurent Laville <laurent.laville@gmail.com>
2021-03-22 09:48:40 +0000

    fixes pdo drivers specific (related to issue #72)

--- tests/Reference/Extension/PhpBundle/Pdo/PdoExtensionTest.php
+++ tests/Reference/Extension/PhpBundle/Pdo/PdoExtensionTest.php
@@ -38,6 +38,44 @@ class PdoExtensionTest extends GenericTest
             'PDOStatement::__wakeup',
         ];
 
+        self::$ignoredconsts = [
+            // @see https://github.com/php/php-src/commit/ed2f6510da7c68b5690c60344cbb9fe73241592a
+            'PDO::SQLITE_ATTR_OPEN_FLAGS',
+            'PDO::SQLITE_OPEN_READONLY',
+            'PDO::SQLITE_OPEN_READWRITE',
+            'PDO::SQLITE_OPEN_CREATE',
+            // @see https://github.com/php/php-src/commit/6f9ebe677bf31d0c64046f18e6b0ac75340cb93e
+            'PDO::SQLITE_ATTR_READONLY_STATEMENT',
+            // @see https://github.com/php/php-src/commit/b546ae986a6efe4daadd23e27f6ccaac5c857e5e
+            'PDO::SQLITE_ATTR_EXTENDED_RESULT_CODES',
+            // @see https://github.com/microsoft/msphpsql/blob/v5.9.0/source/pdo_sqlsrv/pdo_init.cpp#L296
+            'PDO::SQLSRV_ATTR_ENCODING',
+            'PDO::SQLSRV_ATTR_QUERY_TIMEOUT',
+            'PDO::SQLSRV_ATTR_DIRECT_QUERY',
+            'PDO::SQLSRV_ATTR_CURSOR_SCROLL_TYPE',
+            'PDO::SQLSRV_ATTR_CLIENT_BUFFER_MAX_KB_SIZE',
+            'PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE',
+            'PDO::SQLSRV_ATTR_FETCHES_DATETIME_TYPE',
+            'PDO::SQLSRV_ATTR_FORMAT_DECIMALS',
+            'PDO::SQLSRV_ATTR_DECIMAL_PLACES',
+            'PDO::SQLSRV_ATTR_DATA_CLASSIFICATION',
+            'PDO::SQLSRV_PARAM_OUT_DEFAULT_SIZE',
+            'PDO::SQLSRV_ENCODING_DEFAULT',
+            'PDO::SQLSRV_ENCODING_SYSTEM',
+            'PDO::SQLSRV_ENCODING_BINARY',
+            'PDO::SQLSRV_ENCODING_UTF8',
+            'PDO::SQLSRV_CURSOR_STATIC',
+            'PDO::SQLSRV_CURSOR_DYNAMIC',
+            'PDO::SQLSRV_CURSOR_KEYSET',
+            'PDO::SQLSRV_CURSOR_BUFFERED',
+            // @see https://github.com/microsoft/msphpsql/blob/v5.9.0/source/pdo_sqlsrv/pdo_init.cpp#L161-L165
+            'PDO::SQLSRV_TXN_READ_UNCOMMITTED',
+            'PDO::SQLSRV_TXN_READ_COMMITTED',
+            'PDO::SQLSRV_TXN_REPEATABLE_READ',
+            'PDO::SQLSRV_TXN_SERIALIZABLE',
+            'PDO::SQLSRV_TXN_SNAPSHOT',
+        ];
+
         parent::setUpBeforeClass();
     }
 }
