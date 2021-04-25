34015e60330e96b5ebe30c9a7e042fd4ceedf77b
Laurent Laville <laurent.laville@gmail.com>
2021-04-25 08:13:24 +0000

    fixed snmp extension related to specific version 0.1 (see ExtensionVersionProviderTrait)

--- src/Domain/Factory/ExtensionVersionProviderTrait.php
+++ src/Domain/Factory/ExtensionVersionProviderTrait.php
@@ -96,6 +96,7 @@ trait ExtensionVersionProviderTrait
             return '1.2.1';
         } elseif ('snmp' === $normalizedName) {
             if (version_compare(PHP_VERSION, '7.3.0alpha2', 'lt')) {
+                // @see commit https://github.com/php/php-src/commit/70f41d1d9cb03f76f73e7a6099bfc7ce0c2b2701
                 return '0.1';
             }
         }
