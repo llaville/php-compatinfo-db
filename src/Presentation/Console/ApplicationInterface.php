b204b9a66afeb550731bdb031b1df74e8f4d47a5
Laurent Laville <laurent.laville@gmail.com>
2021-07-17 11:14:29 +0000

    back to dev

--- src/Presentation/Console/ApplicationInterface.php
+++ src/Presentation/Console/ApplicationInterface.php
@@ -23,7 +23,7 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 interface ApplicationInterface extends ContainerAwareInterface
 {
     public const NAME = 'Database handler for CompatInfo';
-    public const VERSION = '3.8.0';
+    public const VERSION = '3.9.x-dev';
 
     /**
      * @param CommandLoaderInterface $commandLoader
