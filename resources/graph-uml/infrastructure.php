1b52f58bef9d958da959cae9f5754933874d1b22
Laurent Laville <laurent.laville@gmail.com>
2021-12-29 15:08:41 +0000

    update gh-pages deployment workflow to generate dynamically UML graphs of architecture

---
+++ resources/graph-uml/infrastructure.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * @since Release 3.17.0
+ * @author Laurent Laville
+ */
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+require_once __DIR__ . '/Graph.php';
+
+$dataSource = dirname(__DIR__, 2) . '/src/Infrastructure';
+$paths = ['Bus', 'Framework', 'Persistence'];
+
+Graph::from($dataSource, $paths, basename(__FILE__, '.php'), $_SERVER['argv'][1] ?? null);
