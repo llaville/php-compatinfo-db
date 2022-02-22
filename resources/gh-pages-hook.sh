935c5ef409c9926278fb4cd21805c42b5616862d
Laurent Laville <laurent.laville@gmail.com>
2022-02-22 16:48:02 +0000

    update gh-pages hook script to generate UML class diagrams

--- resources/gh-pages-hook.sh
+++ resources/gh-pages-hook.sh
@@ -1,11 +1,17 @@
-php resources/graph-uml/build.php application-command docs/assets/images
-php resources/graph-uml/build.php application-event docs/assets/images
-php resources/graph-uml/build.php application-query docs/assets/images
-php resources/graph-uml/build.php application-service docs/assets/images
-php resources/graph-uml/build.php domain-factory docs/assets/images
-php resources/graph-uml/build.php domain-repository docs/assets/images
-php resources/graph-uml/build.php domain-valueobject docs/assets/images
-php resources/graph-uml/build.php infrastructure-bus docs/assets/images
-php resources/graph-uml/build.php infrastructure-framework docs/assets/images
-php resources/graph-uml/build.php infrastructure-persistence docs/assets/images
-php resources/graph-uml/build.php presentation-console docs/assets/images
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ASSETS_IMAGE_DIR="docs/assets/images"
+
+php $SCRIPT_DIR/graph-uml/build.php application-command $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php application-event $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php application-query $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php application-service $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php domain-factory $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php domain-repository $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php domain-valueobject $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php infrastructure-bus $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php infrastructure-framework $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php infrastructure-persistence $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/graph-uml/build.php presentation-console $ASSETS_IMAGE_DIR
