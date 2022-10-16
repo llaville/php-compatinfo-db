llaville/php-compatinfo-db

commit 92d2550a1e428f15b20583b99d6381948b712318

raise doctrine/orm constraint to remove deprecated Doctrine\ORM\Tools\Setup

src/Infrastructure/Persistence/Doctrine/EntityManagerFactory.php
@@ -11,8 +11,8 @@ use Doctrine\Common\Cache\Cache;
 use Doctrine\Common\Proxy\AbstractProxyFactory;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\ORMException;
-use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\ORMSetup;
 
 use function getenv;
 use function implode;
@@ -37,7 +37,7 @@ final class EntityManagerFactory
     public static function create(array $connection, bool $isDevMode, string $proxyDir, ?Cache $cache = null): EntityManagerInterface
     {
         $paths = [implode(DIRECTORY_SEPARATOR, [__DIR__, 'Entity'])];
-        $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cache);
+        $config = ORMSetup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cache);
         if ($isDevMode) {
             // suggested for DEV mode: see Doctrine ORM documentation
             // at https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/reference/advanced-configuration.html#auto-generating-proxy-classes-optional

commit 0af362dc03348b355940d945a65e076fe8b4e8ff

fix signature error introduced by commit 92d2550a1e428f15b20583b99d6381948b712318 on doctrine/orm upgrade to 2.12

src/Infrastructure/Persistence/Doctrine/EntityManagerFactory.php
@@ -7,12 +7,13 @@
  */
 namespace Bartlett\CompatInfoDb\Infrastructure\Persistence\Doctrine;
 
-use Doctrine\Common\Cache\Cache;
 use Doctrine\Common\Proxy\AbstractProxyFactory;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\ORMSetup;
+
+use Psr\Cache\CacheItemPoolInterface;
 
 use function getenv;
 use function implode;
@@ -28,13 +29,9 @@ final class EntityManagerFactory
 {
     /**
      * @param array<string, string> $connection
-     * @param bool $isDevMode
-     * @param string $proxyDir
-     * @param Cache|null $cache
-     * @return EntityManagerInterface
      * @throws ORMException
      */
-    public static function create(array $connection, bool $isDevMode, string $proxyDir, ?Cache $cache = null): EntityManagerInterface
+    public static function create(array $connection, bool $isDevMode, string $proxyDir, ?CacheItemPoolInterface $cache = null): EntityManagerInterface
     {
         $paths = [implode(DIRECTORY_SEPARATOR, [__DIR__, 'Entity'])];
         $config = ORMSetup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cache);