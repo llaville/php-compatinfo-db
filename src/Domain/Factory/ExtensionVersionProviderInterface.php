8757eeffc98a1c244b3873499d99e899a866a9a7
Laurent Laville <laurent.laville@gmail.com>
2023-02-25 10:24:32 +0000

    Added support for PHP 8.2.3

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -27,5 +27,5 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_7_4 = '7.4.33';
     public const LATEST_PHP_8_0 = '8.0.28';
     public const LATEST_PHP_8_1 = '8.1.16';
-    public const LATEST_PHP_8_2 = '8.2.1';
+    public const LATEST_PHP_8_2 = '8.2.3';
 }
