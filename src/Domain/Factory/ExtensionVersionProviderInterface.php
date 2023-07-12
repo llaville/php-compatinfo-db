7c686e2a15b9bca89c668270c9bd7350e5d7191c
Laurent Laville <laurent.laville@gmail.com>
2023-07-12 06:04:23 +0000

    add PHP 8.1.21 support

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -26,6 +26,6 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_7_3 = '7.3.33';
     public const LATEST_PHP_7_4 = '7.4.33';
     public const LATEST_PHP_8_0 = '8.0.29';
-    public const LATEST_PHP_8_1 = '8.1.20';
+    public const LATEST_PHP_8_1 = '8.1.21';
     public const LATEST_PHP_8_2 = '8.2.7';
 }
