d7ea3987581c81d2b36081b845b57d8e82aa17d3
Laurent Laville <laurent.laville@gmail.com>
2023-12-06 14:54:44 +0000

    fixed link on about command output

--- src/Presentation/Console/Command/AboutCommand.php
+++ src/Presentation/Console/Command/AboutCommand.php
@@ -45,7 +45,7 @@ final class AboutCommand extends AbstractCommand implements CommandInterface
         /** @var ApplicationInterface $app */
         $app = $this->getApplication();
 
-        $defaultVersion = '6.x';
+        $defaultVersion = '6.0';
 
         $lines = [
             sprintf(
