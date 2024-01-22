13f5a9b3a898db33d5ef0202a263964a5901929e
Laurent Laville <laurent.laville@gmail.com>
2024-01-22 06:45:33 +0000

    prepare feature relase 6.2

--- src/Presentation/Console/Command/AboutCommand.php
+++ src/Presentation/Console/Command/AboutCommand.php
@@ -45,7 +45,7 @@ final class AboutCommand extends AbstractCommand implements CommandInterface
         /** @var ApplicationInterface $app */
         $app = $this->getApplication();
 
-        $defaultVersion = '6.1';
+        $defaultVersion = '6.2';
 
         $lines = [
             sprintf(
