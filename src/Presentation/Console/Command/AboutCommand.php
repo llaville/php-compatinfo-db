66dcd5c943b3f7eb8ce29e8bbc9139d82bcac45b
Laurent Laville <laurent.laville@gmail.com>
2024-03-17 08:15:30 +0000

    fix about command for upcoming release 6.4

--- src/Presentation/Console/Command/AboutCommand.php
+++ src/Presentation/Console/Command/AboutCommand.php
@@ -45,7 +45,7 @@ final class AboutCommand extends AbstractCommand implements CommandInterface
         /** @var ApplicationInterface $app */
         $app = $this->getApplication();
 
-        $defaultVersion = '6.3';
+        $defaultVersion = '6.4';
 
         $lines = [
             sprintf(
