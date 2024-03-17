282f27d73d926aa527099a5f049d01f6a69942b5
Laurent Laville <laurent.laville@gmail.com>
2024-03-17 07:51:28 +0000

    add support to PHP 8.3.4

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -28,5 +28,5 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_8_0 = '8.0.30';
     public const LATEST_PHP_8_1 = '8.1.27';
     public const LATEST_PHP_8_2 = '8.2.17';
-    public const LATEST_PHP_8_3 = '8.3.3';
+    public const LATEST_PHP_8_3 = '8.3.4';
 }
