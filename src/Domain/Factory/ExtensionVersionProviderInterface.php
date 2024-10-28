39235a365d8e9a14f0e77514b454fea4a52eb46a
Laurent Laville <laurent.laville@gmail.com>
2024-10-28 15:58:29 +0000

    added PHP 8.3.13 support

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -28,5 +28,5 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_8_0 = '8.0.30';
     public const LATEST_PHP_8_1 = '8.1.30';
     public const LATEST_PHP_8_2 = '8.2.25';
-    public const LATEST_PHP_8_3 = '8.3.12';
+    public const LATEST_PHP_8_3 = '8.3.13';
 }
