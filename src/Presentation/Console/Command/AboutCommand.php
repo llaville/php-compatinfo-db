8e551d7165aab8249bd5a36186000cbf6f8f69d1
Laurent Laville <laurent.laville@gmail.com>
2024-11-22 11:13:03 +0000

    bump version on about command

--- src/Presentation/Console/Command/AboutCommand.php
+++ src/Presentation/Console/Command/AboutCommand.php
@@ -45,7 +45,7 @@ final class AboutCommand extends AbstractCommand implements CommandInterface
         /** @var ApplicationInterface $app */
         $app = $this->getApplication();
 
-        $defaultVersion = '6.12';
+        $defaultVersion = '6.13';
 
         $lines = [
             sprintf(
