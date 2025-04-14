e1c738c842a498f599ef7e2ccf945e8a158e9cab
Laurent Laville <laurent.laville@gmail.com>
2025-04-14 04:11:32 +0000

    added support to PHP 8.1.32

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -26,7 +26,7 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_7_3 = '7.3.33';
     public const LATEST_PHP_7_4 = '7.4.33';
     public const LATEST_PHP_8_0 = '8.0.30';
-    public const LATEST_PHP_8_1 = '8.1.31';
+    public const LATEST_PHP_8_1 = '8.1.32';
     public const LATEST_PHP_8_2 = '8.2.27';
     public const LATEST_PHP_8_3 = '8.3.17';
     public const LATEST_PHP_8_4 = '8.4.4';
