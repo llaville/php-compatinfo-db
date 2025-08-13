e4239168b3723dfe08284ac868eb55573af13af4
Laurent Laville <laurent.laville@gmail.com>
2025-08-13 17:02:41 +0200

    introduces a pre-commit hook (disabled by default) for CaptainHook that run ECS

---
+++ .github/linters/ecs.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+$projectDir = dirname(__DIR__, 2);
+
+return ECSConfig::configure()
+    ->withPaths([
+        $projectDir . '/config',
+        $projectDir . '/examples',
+        $projectDir . '/resources',
+        $projectDir . '/src',
+        $projectDir . '/tests',
+    ])
+
+    // add a single rule
+    ->withRules([
+        NoUnusedImportsFixer::class,
+    ])
+
+    // add sets - group of rules, from easiest to more complex ones
+    ->withPreparedSets(
+        spaces: true,
+    )
+;
