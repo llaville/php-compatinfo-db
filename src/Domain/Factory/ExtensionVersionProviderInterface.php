34205b9a5224d21b03d9058559d9cdc34c3dc9f6
Laurent Laville <laurent.laville@gmail.com>
2025-12-19 13:26:13 +0000

    add support to PHP 8.2.30

--- src/Domain/Factory/ExtensionVersionProviderInterface.php
+++ src/Domain/Factory/ExtensionVersionProviderInterface.php
@@ -27,7 +27,7 @@ interface ExtensionVersionProviderInterface
     public const LATEST_PHP_7_4 = '7.4.33';
     public const LATEST_PHP_8_0 = '8.0.30';
     public const LATEST_PHP_8_1 = '8.1.33';
-    public const LATEST_PHP_8_2 = '8.2.29';
+    public const LATEST_PHP_8_2 = '8.2.30';
     public const LATEST_PHP_8_3 = '8.3.28';
     public const LATEST_PHP_8_4 = '8.4.15';
     public const LATEST_PHP_8_5 = '8.5.0';
